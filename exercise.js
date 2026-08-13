defd77ae73390ef74915e6e242d9708313e10b9e
Steve Benton <steve@bentonemail.uk>
2026-08-13 12:16:37 +0100

    update dom challenge code

--- exercise.js
+++ exercise.js
@@ -3,24 +3,34 @@
 // 1. Select the <h1> element by "drilling into the DOM" and
 //    save it in a variable with a name of your choice
 let headerElement = document.body.children[1].children[0];
+// Alternative:
+// headerElement = document.body.children[1].firstElementChild;
+console.dir(headerElement);
 
 // 2. Use the variable from (1) and get access to the "parent"
 //    element of the stored <h1> element (i.e. to the <body> element)
 //    BONUS: Try using the variable from (1) to get access to the
 //    sibling element (i.e. the <p> element next to the <h1> element)
 
-let parentElement = document.body.children[1];
-let siblingElement = document.body.children[1].children[1];
+let mainElement = headerElement.parentElement;
+console.dir(mainElement);
+
+let paraElement = headerElement.nextElementSibling;
+console.dir(paraElement);
 
 // 3. Select the <h1> element with getElementById and store in
 //    the same or a new variable (up to you)
 let headerElement2 = document.getElementById("main-heading");
+console.dir(headerElement2);
 
 // 4. Select the second <p> element with querySelector (you might
 //    need to add something in the HTML code, e.g. a class)
 //    and store it in a new variable with a name of your choice
 
 let secondPara = document.querySelectorAll("p")[1];
+//Alternative
+secondPara = document.querySelectorAll(".link-section");
+console.dir(secondPara);
 
 // 5. BONUS TASK: Try changing the text content of the <p> element
 //    you selected in (4) and set it to any other text of your choice

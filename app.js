354c6cfd96d6650c63efbfb68c716eb98e202a01
Steve Benton <steve@bentonemail.uk>
2026-08-14 10:59:48 +0100

    add new <a> element with JS

--- app.js
+++ app.js
@@ -17,5 +17,12 @@ anchorElement.href = "https://www.readingfc.co.uk/";
 
 // ADD AN ELEMENT
 // 1. Create the new element
+let newAnchorElement = document.createElement("a");
+newAnchorElement.innerText = "MDN DOM Material";
+newAnchorElement.href = "https://developer.mozilla.org/en-US/docs/Glossary/DOM";
+
 // 2. Get access to the parent element that should hold the new element
+let firstParagraph = document.querySelector("p");
+
 // 3. Insert the new element into the parent element content
+firstParagraph.append(newAnchorElement);

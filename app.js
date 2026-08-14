8cd6e32e54eb9f03ef28b6ccd391b0fcc4d6dbfc
Steve Benton <steve@bentonemail.uk>
2026-08-14 13:00:52 +0100

    add use event object

--- app.js
+++ app.js
@@ -4,6 +4,20 @@ let paragraphElement = document.querySelector("p");
 
 function changeParagraphText() {
   paragraphElement.innerText = "Clicked!";
+  console.log("Paragrah Clicked");
 }
 
 paragraphElement.addEventListener("click", changeParagraphText);
+
+let inputElement = document.querySelector("input");
+
+function readUserInput(event) {
+  let enteredText = inputElement.value;
+  console.log(`Item Entered in Input: ${enteredText}`);
+  console.log(event);
+  console.log(event.type); //Event type
+  console.log(event.data); //Single Value Entered
+  console.log(event.target.value); //Cumulative Value Entered
+}
+
+inputElement.addEventListener("input", readUserInput);

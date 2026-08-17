c75841d0d157e0b7d8118cc719fe543224532f2f
Steve Benton <steve@bentonemail.uk>
2026-08-17 13:06:10 +0100

    add use of classList.add/remove to change colors

--- demo.js
+++ demo.js
@@ -6,12 +6,18 @@ const remainingCharsElement = document.getElementById("remaining-chars");
 const maxAllowedChars = productNameInputElement.maxLength;
 
 function updateRemainingCharacters(event) {
-  remainingCharsElement.style.color = "green";
+  // remainingCharsElement.style.color = "green";
+  remainingCharsElement.classList.remove("warning");
+  productNameInputElement.classList.remove("warning");
+
   const enteredText = event.target.value;
   const charsLeft = maxAllowedChars - enteredText.length;
   remainingCharsElement.innerText = charsLeft;
+
   if (charsLeft < 10) {
-    remainingCharsElement.style.color = "red";
+    // remainingCharsElement.style.color = "red";
+    remainingCharsElement.classList.add("warning");
+    productNameInputElement.classList.add("warning");
   }
   console.log(charsLeft);
 }
